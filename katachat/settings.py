6ce83102dd41dfc5f9530622571f068c4f9810a0
Levy <leskeylevy@gmail.com>
2018-10-24 19:38:59 +0300

    managed to make my chat server to be fully asynchronus and gues what it actualy works, yay me

--- katachat/settings.py
+++ katachat/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'channels',
+    'chat',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -118,3 +120,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# channels
+
+ASGI_APPLICATION = 'katachat.routing.application'
+
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
+}
